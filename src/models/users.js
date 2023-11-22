0a8256f6f54878bd35b221cd57727b58247f059c
stasikrus <stas-boss@mail.ru>
2023-11-22 13:08:13 +0300

    Добавит хеширование пароля

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {type: String, required: true, unique: true},
@@ -6,8 +7,16 @@ const userSchema = new Schema({
     email: {type: String, required: true}
 });
 
-userSchema.methods.validatePassword = function(candidatePassword) {
-    return candidatePassword === this.password;
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.methods.validatePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
 }
 
 module.exports = model('User', userSchema);
